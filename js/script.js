bb437daf4d9ac9aa75d5c6d20732d4301c5812dc
moretti-matteo <matteomoretti2000@hotmail.it>
2022-05-02 15:24:13 +0200

    modifica condizioni if

--- js/script.js
+++ js/script.js
@@ -2,18 +2,19 @@ const element = document.querySelector(".container");
 console.log(element);
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 !== 0) {
-        element.innerHTML += `<span class="elem green">Fizz</span>`;
-        console.log("Fizz", i);
+    
+    if (i % 5 === 0 && i % 3 === 0) {
+        element.innerHTML += `<span class="elem red">FizzBuzz</span>`;
+        console.log("FizzBuzz", i);
 
-    } else if (i % 5 === 0 && i % 3 !== 0) {
+    } else if (i % 5 === 0) {
         element.innerHTML += `<span class="elem yellow">Buzz</span>`;
         console.log("Buzz", i);
 
-    } else if (i % 5 === 0 && i % 3 === 0) {
-        element.innerHTML += `<span class="elem red">FizzBuzz</span>`;
-        console.log("FizzBuzz", i);
-        
+    } else if (i % 3 === 0) {
+        element.innerHTML += `<span class="elem green">Fizz</span>`;
+        console.log("Fizz", i);
+
     } else {
         element.innerHTML += `<span class="elem blue">${i}</span>`;
         console.log(i);
