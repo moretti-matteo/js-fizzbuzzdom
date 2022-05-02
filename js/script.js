75eaa7f95404b5baf537338f6f8bc1a2c48934f4
moretti-matteo <matteomoretti2000@hotmail.it>
2022-05-02 15:56:33 +0200

    Bonus

--- js/script.js
+++ js/script.js
@@ -1,22 +1,35 @@
-const element = document.querySelector(".container");
-console.log(element);
+const container = document.querySelector(".container");
 
 for (let i = 1; i <= 100; i++) {
-    
+    const span = document.createElement("span");
+
     if (i % 5 === 0 && i % 3 === 0) {
-        element.innerHTML += `<span class="elem red">FizzBuzz</span>`;
+        // container.innerHTML += `<span class="elem red">FizzBuzz</span>`;
+        span.classList.add("elem","red");
+        span.innerText = "FizzBuzz";
+        container.append(span);
         console.log("FizzBuzz", i);
 
     } else if (i % 5 === 0) {
-        element.innerHTML += `<span class="elem yellow">Buzz</span>`;
+        // container.innerHTML += `<span class="elem yellow">Buzz</span>`;
+        span.classList.add("elem","yellow");
+        span.innerText = "Buzz";
+        container.append(span);
         console.log("Buzz", i);
 
     } else if (i % 3 === 0) {
-        element.innerHTML += `<span class="elem green">Fizz</span>`;
+        // container.innerHTML += `<span class="elem green">Fizz</span>`;
+        span.classList.add("elem","green");
+        span.innerText = "Fizz";
+        container.append(span);
         console.log("Fizz", i);
 
     } else {
-        element.innerHTML += `<span class="elem blue">${i}</span>`;
+        // container.innerHTML += `<span class="elem blue">${i}</span>`;
+        span.classList.add("elem","blue");
+        span.innerText = i;
+        container.append(span);
         console.log(i);
+
     }
 }
