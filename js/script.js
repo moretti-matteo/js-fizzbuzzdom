bf2501d8c5b10170fb82bbdf083f269e113dff93
moretti-matteo <matteomoretti2000@hotmail.it>
2022-05-02 15:19:45 +0200

    milestone 2-3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
+const element = document.querySelector(".container");
+console.log(element);
+
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 !== 0) {
+        element.innerHTML += `<span class="elem green">Fizz</span>`;
         console.log("Fizz", i);
+
     } else if (i % 5 === 0 && i % 3 !== 0) {
+        element.innerHTML += `<span class="elem yellow">Buzz</span>`;
         console.log("Buzz", i);
+
     } else if (i % 5 === 0 && i % 3 === 0) {
+        element.innerHTML += `<span class="elem red">FizzBuzz</span>`;
         console.log("FizzBuzz", i);
+        
     } else {
+        element.innerHTML += `<span class="elem blue">${i}</span>`;
         console.log(i);
     }
 }
